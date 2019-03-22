2f5817440d41a5f66a55c84884c24ae1ad216398
DIEGO JUAN NASARENO LUPI <djl0112@lab10.famaf.unc.edu.ar>
2019-03-22 16:49:28 -0300

    NGramGenerator init

--- languagemodeling/ngram_generator.py
+++ languagemodeling/ngram_generator.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import random
+import operator
 
 
 class NGramGenerator(object):
@@ -12,13 +13,24 @@ class NGramGenerator(object):
 
         # compute the probabilities
         probs = defaultdict(dict)
-        # WORK HERE!!
-
+        keys = model._count.keys()
+        dic_filtered = [elem for elem in keys if len(elem)==self._n]
+        for elem in dic_filtered:
+            prev_tokens = elem[0:self._n - 1]
+            token = elem[self._n - 1]
+            prob = model.cond_prob(token,prev_tokens)
+            lis = list(probs[prev_tokens].items())+[(token,prob)]
+            probs[prev_tokens] = dict(lis + [(token,prob)])
+        print(probs)
         self._probs = dict(probs)
 
         # sort in descending order for efficient sampling
-        self._sorted_probs = sorted_probs = {}
-        # WORK HERE!!
+        self._sorted_probs = sorted_probs = defaultdict(dict)
+        for e, b in self._probs.items():
+            sort = sorted(b.items(), key=operator.itemgetter(1))
+            sorted_probs[e] = sort 
+        self._sorted_probs = sorted_probs
+    
 
     def generate_sent(self):
         """Randomly generate a sentence."""
